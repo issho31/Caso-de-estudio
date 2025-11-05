0fa92fc597b6d8e21afae576fca9b7b52576a24c
Frank Leonardo Silva Castillo <frannsito110@gmail.com>
2025-11-05 10:09:03 -0500

    Clase Ingrediente

---
+++ Ingrediente.cpp
@@ -0,0 +1,33 @@
+#include "Ingrediente.h"
+
+Ingrediente::Ingrediente(std::string nom, std::string unidad, double cantidad, double minimo)
+    : nombre(nom), unidadMedida(unidad), cantidadDisponible(cantidad), nivelMinimo(minimo) {}
+
+std::string Ingrediente::getNombre() const {
+    return nombre;
+}
+
+std::string Ingrediente::getUnidad() const {
+    return unidadMedida;
+}
+
+double Ingrediente::getCantidad() const {
+    return cantidadDisponible;
+}
+
+double Ingrediente::getNivelMinimo() const {
+    return nivelMinimo;
+}
+
+void Ingrediente::setCantidad(double cantidad) {
+    cantidadDisponible = cantidad;
+}
+
+bool Ingrediente::consumir(double cantidad) {
+    cantidadDisponible -= cantidad;
+    return true;
+}
+
+bool Ingrediente::estaDebajoDeMinimoStock() const {
+    return cantidadDisponible < nivelMinimo;
+}
