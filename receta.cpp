dc2d0aded80ec696d64f72a96bfe14faf2345670
issho31 <jdiego.16c@gmail.com>
2025-11-11 20:42:09 -0500

    Corregir Validad cantidades positivas Receta.cpp

--- receta.cpp
+++ receta.cpp
@@ -7,10 +7,11 @@ std::string Receta::getNombre() const {
 }
 
 void Receta::agregarIngrediente(std::string nombreIng, double cantidad) {
-    // Error: no valida que la cantidad sea positiva
-    ingredientes[nombreIng] = cantidad;
+    // Correcion ahora evita agregar ingredientes con cantidad negativa
+    if (cantidad > 0) {
+        ingredientes[nombreIng] = cantidad;
+    }
 }
-
 std::map<std::string, double> Receta::getIngredientes() const {
     return ingredientes;
 }
